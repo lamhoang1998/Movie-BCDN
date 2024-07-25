f712584af6538fe28af80de6028aaacd527ce580
Lam <lamkyo373@gmail.com>
2024-07-25 19:15:18 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import GameXucSac from "./GameXucSac";
+
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<GameXucSac />
+		</>
+	);
+}
+
+export default App;
